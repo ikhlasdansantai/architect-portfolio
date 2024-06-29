c46dc9057ee5562ce26eb86f8fe2167a19fae6fb
ikhlasdansantai <initesting5@gmail.com>
2024-06-29 23:02:20 +0800

    setup: initialize project with react-mui

---
+++ src/theme.tsx
@@ -0,0 +1,41 @@
+import { createTheme, responsiveFontSizes } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#5313CA",
+    },
+    secondary: {
+      main: "#FF9963",
+    },
+    ascent: {
+      main: "#75F1F1",
+      dark: "#050729",
+    },
+    action: {
+      hover: "#EDEEF2",
+      active: "#EDEEF2",
+    },
+    background: {
+      default: "#fff",
+      paper: "#EDEEF2",
+    },
+  },
+  breakpoints: {
+    values: {
+      xs: 0,
+      smallPhone: 350,
+      sm: 600,
+      md: 860,
+      lg: 1200,
+      xl: 1536,
+    },
+  },
+  typography: {
+    h1: {
+      fontSize: 84,
+    },
+  },
+});
+
+export default responsiveFontSizes(theme);
---
+++ src/types.ts
@@ -0,0 +1,22 @@
+export interface PartnersItem {
+  icon: JSX.Element | string;
+  title: string;
+}
+
+export interface PriceCardProps {
+  title: string;
+  price: string;
+  description: string;
+  benefits: string[];
+}
+
+declare module "@mui/material/styles" {
+  interface BreakpointOverrides {
+    xs: true;
+    sm: true;
+    md: true;
+    lg: true;
+    xl: true;
+    smallPhone: true;
+  }
+}
